4f23b69733689308cf8a330550806e2f659d1e9f
JoLeaper <joleap123@gmail.com>
2020-06-08 13:14:15 -0700

    four tests written for app.test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,46 @@
 const request = require('supertest');
 const app = require('../lib/app');
+// res.body => JSON
+// res.text => text
 
 describe('createResponse', () => {
+    it('handles the / route', () => {
+        return request(app)
+        .get('/')
+        .then(res => {
+            expect(res.something).toEqual('hi');
+        });
+    });
 
+    it('handles the /echo route', () => {
+        return request(app)
+        .post('/echo')
+        .then(res => {
+            expect(res.text).toEqual('');
+        });
+    });
+
+    it('handles the /red route', () => {
+        return request(app)
+        .get('/red')
+        .then(res => {
+            expect(res.something).toEqual('<h1>red</h1>');
+        });
+    });
+
+    it('handles the /green route', () => {
+        return request(app)
+        .get('/green')
+        .then(res => {
+            expect(res.something).toEqual('<h1>green</h1>');
+        });
+    });
+
+    it('handles the /blue route', () => {
+        return request(app)
+        .get('/blue')
+        .then(res => {
+            expect(res.something).toEqual('<h1>blue</h1>');
+        });
+    });
 });
